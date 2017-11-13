484cebdf3ff4af115486a966b8190523689e7d58
soong@company@shi6w1120 <songxinkun2003@sina.com>
2017-11-13 18:54:23 +0800

    add dummy page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id='root'>
+    <transition name='fade' mode='out-in'>
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  components: {
+  }
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="scss">
+body {
+  margin: 0px;
+  padding: 0px;
+  /*background: url(assets/bg1.jpg) center !important;
+    background-size: cover;*/
+  // background: #1F2D3D;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+  font-size: 14px;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+#root {
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  width: 100%;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all .2s ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-form :model='account' :rules="rules" ref="loginForm" label-position="left" label-width="0px" class="login-container">
+    <h3 class="title">Login</h3>
+    <el-form-item prop="username">
+      <el-input type="text" v-model="account.username" auto-complete="off" placeholder="user name"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input type="password" v-model="account.password" auto-complete="off" placeholder="password"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="rememberMe" checked class="remember">Remember me</el-checkbox>
+    <el-form-item style="width:100%;">
+      <el-button type="primary" style="width:100%;" @click.native.prevent="login" :loading="logining">Login</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      logining: false,
+      account: {
+        username: 'admin',
+        password: '123456'
+      },
+      rules: {
+        username: [
+          {required: true, message:'please input user name', trigger: 'blur'},
+        ],
+        password:[
+          {required: true, message:'please input password', trigger: 'blur'},
+        ],
+      },
+      rememberMe: true
+    }
+  },
+
+  methods:{
+    collapse: function() {
+      this.collapsed = !this.collapsed;
+    },
+
+    login: function(event) {
+      var self = this;
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          self.logining = true;
+          console.log('login succeed!');
+          self.logining = false;
+
+          self.$router.push({ path: '/' });
+        }else{
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+
+  }
+}
+
+</script>
+
+
+<<style scoped lang='scss'>
+  .login-container {
+    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 0px auto 40px auto;
+      text-align: center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 35px 0px;
+    }
+  }
+
+</style>
